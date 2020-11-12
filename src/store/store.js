33c3010009fec4c3c14e986d4365f2a8639c5819
izqalan <izqalan@gmail.com>
2020-11-13 03:21:31 +0800

    added designs screenshots

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import formBuildReducer from '../redux/formBuilder/index';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     formData: formBuildReducer,
   },
 });
